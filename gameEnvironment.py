0f94fb3639795e56aa673689c20937aefc84477e
namekridchai <66484292+namekridchai@users.noreply.github.com>
2023-06-11 22:57:46 +0700

    first commit

---
+++ gameEnvironment.py
@@ -0,0 +1,17 @@
+class player:
+    def __init__(self, name):
+        self.name = name
+        self.matchScore  
+        self.gameScore  
+        self.bidStatus = False
+    
+class Game:
+    def initializeScore():
+        pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
