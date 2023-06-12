ff407263c6a2df39b23df01da7bf24c1ccce5fa0
namekridchai <66484292+namekridchai@users.noreply.github.com>
2023-06-12 11:59:25 +0700

    add testing class for player class

---
+++ gameEnvironment/testGameEngine/testPlayer.py
@@ -0,0 +1,16 @@
+import unittest
+import sys
+sys.path.append('C:\\Users\\User\\Desktop\\gameEnvironment\\gameEnvironment\\gameEngine')
+
+from gameEnvironment import *
+
+
+class testPlayer(unittest.TestCase):
+    def test_setGameScore(self):
+        pass
+    def test_getGameScore(self):
+        pass
+
+# Run the tests
+if __name__ == '__main__':
+    unittest.main()
