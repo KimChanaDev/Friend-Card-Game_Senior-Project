12ee6bf081d708b5acb9e6c59b34320928b5c24a
namekridchai <66484292+namekridchai@users.noreply.github.com>
2023-06-12 13:33:54 +0700

    test_set&get GameScore

--- gameEnvironment/testGameEngine/testPlayer.py
+++ gameEnvironment/testGameEngine/testPlayer.py
@@ -7,16 +7,16 @@ from gameEnvironment import *
 
 class testPlayer(unittest.TestCase):
     def test_setGameScore(self):
-        player = player('p1')
-        player.setGameScore(50)
-        self.assertIs(player.getGameScore(),50)
-        self.assertRaises(NegativeIntegerError,player.setGameScore,-42)
-        self.assertRaises(ValueError,player.setGameScore,100)
+        p1 = player('p1')
+        p1.setGameScore(50)
+        self.assertIs(p1.getGameScore(),50)
+        self.assertRaises(ValueError,p1.setGameScore,-42)
+        self.assertRaises(ValueError,p1.setGameScore,101)
     def test_getGameScore(self):
-        player = player('p1')
+        p1 = player('p1')
         score = 50
-        player.setGameScore(score)
-        self.assertIs(player.getGameScore(),score)
+        p1.setGameScore(score)
+        self.assertIs(p1.getGameScore(),score)
 
 # Run the tests
 if __name__ == '__main__':
