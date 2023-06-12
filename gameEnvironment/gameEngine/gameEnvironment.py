f83f5bc249f3f01923ea7d6cce31e5aeee60dbe3
namekridchai <66484292+namekridchai@users.noreply.github.com>
2023-06-12 13:36:19 +0700

    setup game score complete

--- gameEnvironment/gameEngine/gameEnvironment.py
+++ gameEnvironment/gameEngine/gameEnvironment.py
@@ -1,9 +1,10 @@
 class player:
     def __init__(self, name):
-        self.name = name
-        self.matchScore  = 0
-        self.bidStatus = False
-        self.card = []
+        self.__name = name
+        self.__matchScore  = 0
+        self.__gameScore = 0
+        self.__bidStatus = False
+        self.__card = []
     def dropCard():
         pass
     def bid(point):
@@ -12,10 +13,14 @@ class player:
         pass
     def getMatchScore():
         pass
-    def setGameScore():
-        self.gameScore = 0
-    def getGameScore():
-        pass
+    def setGameScore(self,score):
+        if score < 0:
+            raise ValueError("Negative scores are not allowed.")
+        elif score > 100:
+            raise ValueError("Scores greater than 100 are not allowed")
+        self.__gameScore = score
+    def getGameScore(self):
+        return self.__gameScore
     def addCard():
         pass
     def getCard():
@@ -51,21 +56,29 @@ class team:
         pass
 class Game:
     def __init__(self,p1,p2,p3,p4):
-        self.trumpCard
-        self.frienCard
-        self.team
-        self.player1 = p1
-        self.player2 = p2
-        self.player3 = p3
-        self.player4 = p4
+        self.trumpCard = None
+        self.frienCard = None
+        self.team = None
+        self.__players = [None,None,None,None]
+        self.__players[0] = p1
+        self.__players[1] = p2
+        self.__players[2] = p3
+        self.__players[3] = p4
     
-    def setGameScore():
-        player1.setGameScore(0)
-        player2.setGameScore(0)
-        player3.setGameScore(0)
-        player4.setGameScore(0)
-
+    def setGameScore(self):
+        self.getPlayer(0).setGameScore(0)
+        self.getPlayer(1).setGameScore(0)
+        self.getPlayer(2).setGameScore(0)
+        self.getPlayer(3).setGameScore(0)
         pass
+    def getPlayer(self,index):
+        if index < 0:
+            raise ValueError("Negative integers are not allowed.")
+        elif index > 3:
+            raise ValueError("Player indexs greater than 3 are not allowed")
+        return self.__players[index]
+   
+    
 
 
 def main():
