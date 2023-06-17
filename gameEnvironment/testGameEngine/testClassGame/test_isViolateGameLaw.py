3b2ee5561bb1508fa5acbdcf7b0562952b6590be
namekridchai <66484292+namekridchai@users.noreply.github.com>
2023-06-17 20:26:37 +0700

    feature : test_isViolateGameLaw_should_return_true_if_drop_card_is_trump_card

--- gameEnvironment/testGameEngine/testClassGame/test_isViolateGameLaw.py
+++ gameEnvironment/testGameEngine/testClassGame/test_isViolateGameLaw.py
@@ -19,7 +19,22 @@ class test_isViolateGameLaw(unittest.TestCase):
         game.identifyTeam()
         Player = game.getPlayer(game.getBidWinnerPosition())
         card = Player.getAllCard()[0]
-        self.assertTrue(game.isViolateGameLaw(card)) 
+        self.assertTrue(game.isViolateGameLaw(card))
+    def test_isViolateGameLaw_should_return_True_if_drop_card_is_trump_card(self):
+       
+        p1 = player("p1")
+        p2 = player("p2")
+        p3 = player("p3")
+        p4 = player("p4")
+        game = Game(p1,p2,p3,p4)
+        game.setGameScore()
+        game.provideCard()
+        game.determineBidWinner()
+        game.setFriendCard()
+        game.identifyTeam()
+        Player = game.getPlayer(game.getBidWinnerPosition())
+        Card = card(game.getTrumpCard,5,4)
+        self.assertTrue(game.isViolateGameLaw(Card))
 
         
 
