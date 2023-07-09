7465ba5ddebda7284e05db4aefddfa3a157fb0bb
namekridchai <66484292+namekridchai@users.noreply.github.com>
2023-07-09 11:37:11 +0700

    delete unmeaningfull test case

--- gameEnvironment/testGameEngine/testClassGame/test_isViolateGameLaw.py
+++ gameEnvironment/testGameEngine/testClassGame/test_isViolateGameLaw.py
@@ -7,10 +7,10 @@ import unittest
 
 class test_isViolateGameLaw(unittest.TestCase):
     def test_isViolateGameLaw_should_return_True_if_player_is_first_person_to_play(self):
-        p1 = player("p1")
-        p2 = player("p2")
-        p3 = player("p3")
-        p4 = player("p4")
+        p1 = player("p1",0)
+        p2 = player("p2",1)
+        p3 = player("p3",2)
+        p4 = player("p4",3)
         game = Game(p1,p2,p3,p4)
         game.setGameScore()
         game.provideCard()
@@ -20,26 +20,13 @@ class test_isViolateGameLaw(unittest.TestCase):
         Player = game.getPlayer(game.getBidWinnerPosition())
         card = Player.getAllCard()[0]
         self.assertTrue(game.isNotViolateGameLaw(card))
-    def test_isViolateGameLaw_should_return_True_if_drop_card_is_trump_card(self):
-        p1 = player("p1")
-        p2 = player("p2")
-        p3 = player("p3")
-        p4 = player("p4")
-        game = Game(p1,p2,p3,p4)
-        game.setGameScore()
-        game.provideCard()
-        game.determineBidWinner()
-        game.setFriendCard()
-        game.identifyTeam()
-        Player = game.getPlayer(game.getBidWinnerPosition())
-        Card = card(game.getTrumpCard,5,4)
-        self.assertTrue(game.isNotViolateGameLaw(Card))
+    
 
     def test_isViolateGameLaw_should_return_True_if_suite_card_is_the_same_as_first_card_played(self):
-        p1 = player("p1")
-        p2 = player("p2")
-        p3 = player("p3")
-        p4 = player("p4")
+        p1 = player("p1",0)
+        p2 = player("p2",1)
+        p3 = player("p3",2)
+        p4 = player("p4",3)
         game = Game(p1,p2,p3,p4)
         game.setGameScore()
         game.provideCard()
@@ -52,10 +39,10 @@ class test_isViolateGameLaw(unittest.TestCase):
         Card2 = card("Hearts",6,7)
         self.assertTrue(game.isNotViolateGameLaw(Card2))
     def test_isViolateGameLaw_should_return_false_if_suite_card_is_not_the_same_as_first_card_played(self):
-        p1 = player("p1")
-        p2 = player("p2")
-        p3 = player("p3")
-        p4 = player("p4")
+        p1 = player("p1",0)
+        p2 = player("p2",1)
+        p3 = player("p3",2)
+        p4 = player("p4",3)
         game = Game(p1,p2,p3,p4)
         game.setGameScore()
         game.provideCard()
