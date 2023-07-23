ec3ab6080a2d0c44fa97b2d6623945441c4684ef
namekridchai <66484292+namekridchai@users.noreply.github.com>
2023-07-23 20:36:58 +0700

    modify card in playerhand layout

--- FCG_front-end/src/App.jsx
+++ FCG_front-end/src/App.jsx
@@ -3,16 +3,17 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const n = 1
+  const n = 10
   const myArray = new Array(n).fill(0);
   const picStyles = {"width":`${Math.min(100/n,100/3)}%`}
   const cardName = '9_of_clubs.svg'
   const cardPath = "public\\SVG-cards-1.3\\" + cardName
   
   return (
-      <figure>
+      <figure style = {n>10?{paddingInlineStart:`${(n-1)*10}px`}:null}>
         
-        {myArray.map(e=><img src= {cardPath} style = {picStyles}alt="" />)}
+        {myArray.map( (e,i)=><img src= {cardPath} style = {n>10?{...picStyles,"right":i*10}:picStyles}   alt="" />)}
+        
       </figure>
   )
 }
