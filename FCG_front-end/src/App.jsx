3587e516482f287d2044673db1373ef77298bdc5
namekridchai <66484292+namekridchai@users.noreply.github.com>
2023-08-19 22:55:45 +0700

    increse card in hand size

--- FCG_front-end/src/App.jsx
+++ FCG_front-end/src/App.jsx
@@ -1,5 +1,5 @@
 import InGameInterface from "./interface/InGameInterface"
-import './interface/App.css'
+
 function App() {
   return <InGameInterface/>
 }
--- FCG_front-end/src/interface/InGameInterface.jsx
+++ FCG_front-end/src/interface/InGameInterface.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 
-
+import './IngameInterface.css'
 
 function InGameInterface() {
   const n = 13
   const myArray = new Array(n).fill(0);
-  const picStyles = {"width":`${Math.min(100/(n),100/5)}%`}
+  const picStyles = {"width":`${Math.min(100/(n),100/7)}%`}
   const cardName = '9_of_clubs.svg'
   // const cardName = 'back.svg'
   const cardPath = "..\\public\\SVG-cards-1.3\\" + cardName
-  const offset = 0+(13-n)
+  const offset = 10
   return (
     <>
     <figure className='top' >
