c49d966050ec6db8ab0f7bb33bcbe6aedf60d6ed
namekridchai <66484292+namekridchai@users.noreply.github.com>
2023-08-19 18:50:21 +0700

    adjust size of card in hand

--- FCG_front-end/src/interface/InGameInterface.jsx
+++ FCG_front-end/src/interface/InGameInterface.jsx
@@ -3,13 +3,13 @@ import { useState } from 'react'
 
 
 function InGameInterface() {
-  const n = 11
+  const n = 13
   const myArray = new Array(n).fill(0);
-  const picStyles = {"width":`${Math.min(100/n,100/3)}%`}
-  // const cardName = '9_of_clubs.svg'
-  const cardName = 'back.svg'
+  const picStyles = {"width":`${Math.min(100/(n),100/5)}%`}
+  const cardName = '9_of_clubs.svg'
+  // const cardName = 'back.svg'
   const cardPath = "..\\public\\SVG-cards-1.3\\" + cardName
-  
+  const offset = 0+(13-n)
   return (
     <>
     <figure className='top' >
@@ -23,8 +23,8 @@ function InGameInterface() {
     
     </figure>
 
-    <figure className='bot' style = {n>10?{paddingInlineStart:`${(n-1)*10}px`}:null}>
-        {myArray.map( (e,i)=><img src= {cardPath} style = {n>10?{...picStyles,"right":i*10}:picStyles}   alt="" />)}  
+    <figure className='bot' style = {{paddingInlineStart:`${(n-1)*offset}px`}}>
+        {myArray.map( (e,i)=><img src= {cardPath} style = {{...picStyles,"right":i*offset}}   alt="" />)}  
     </figure>
     </>
     
