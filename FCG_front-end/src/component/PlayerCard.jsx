5852d8e66c5e448e7e870f5b0e53cd5c2ad95c2b
namekridchai <66484292+namekridchai@users.noreply.github.com>
2023-10-01 19:53:06 +0700

    complete in lobby v2

--- FCG_front-end/src/component/PlayerCard.jsx
+++ FCG_front-end/src/component/PlayerCard.jsx
@@ -1,14 +1,21 @@
+
 import './PlayerCard.css'
-function PlayerCard({ name, desc ,isInLobby}) {
+function PlayerCard({ name, desc, isInLobby, isLeft }) {
   return (
-    <section className='profile '>
-        <img src="./khonKoHok.jpg" alt="" />
-      <div className='player_info'>
-        <h3 className='player_name'>{name}  </h3>
-        {isInLobby && <button className='kick_button'>❌</button>}      
-        <p className='desc text-sm text-red-600'>{desc}</p>
-      </div>
-    </section>
+ 
+      <section className='profile '>
+        {isInLobby && isLeft && <button className='kick_button_left' style={{ zIndex: 9999 }}>❌</button>}
+       
+        <img src="./khonKoHok.jpg" alt="" style={{ order: isLeft ? 1 : 2, zIndex: isInLobby && 999 }} />
+        <div className='player_info' style={{ order: isLeft ? 2 : 1 }}>
+          <h3 className='player_name'>{name}  </h3>
+          <p className='desc text-sm text-red-600'>{desc}</p>
+        </div>
+        {!isLeft && <button className='kick_button_right' style={{ zIndex: 9999 }}>❌</button>}
+      </section>
+      
+   
+
   )
 }
 export default PlayerCard
--- FCG_front-end/src/interface/InLobby.jsx
+++ FCG_front-end/src/interface/InLobby.jsx
@@ -6,8 +6,11 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import EmojiPanel from '../component/EmojiPanel';
 import LobbyInfo from '../component/LobbyInfo';
 import Menu from '../component/Menu';
+import SlideBar from '../component/SlideBar';
 import './InLobby.css'
+
 function InLobby() {
+  
   // const cardName = 'back.svg'
   return (
     <>
@@ -16,25 +19,20 @@ function InLobby() {
         <SettingsIcon className='setting' sx={{ fontSize: 50 }} />
       </section>
       <section className='left' >
-        <PlayerCard name={'👑 khonKohok1'} desc={'udi:1234'} />
-        <PlayerCard name={'khonKohok2'} desc={'udi:1234'} />
+        <PlayerCard name={'👑 khonKohok1'} desc={'uid:1234'} isLeft={true} isInLobby={true}/>
+        <PlayerCard name={'khonKohok2'} desc={'uid:1234'} isLeft={true} isInLobby={true}/>
       </section>
       <section className='right'>
-        
-        <PlayerCard name={'khonKohok3'} desc={'udi:1234'}/>
-        <PlayerCard name={'khonKohok4'} desc={'udi:1234'}/>
+        <PlayerCard name={'khonKohok3'} desc={'uid:1234'} isInLobby={true}/>
+        <PlayerCard name={'khonKohok4'} desc={'uid:1234'} isInLobby={true}/>
       </section>
       <section className='mid'>
         <Menu/>
       </section>
-      <svg className='slide_bar' xmlns="http://www.w3.org/2000/svg" width="43" height="201" viewBox="0 0 43 201" fill="none">
-        <path d="M0 30C0 13.4315 13.4315 0 30 0H43V201H30C13.4315 201 0 187.569 0 171V30Z" fill="#2A324D" />
-        <path d="M13 100L47.5 68.8231V131.177L13 100Z" fill="white" />
-      </svg>
-      {/* <section className='bot'>
-      < EmojiPanel />
-      </section> */}
+      <SlideBar/>
+     
     </>
   )
 }
+
 export default InLobby
