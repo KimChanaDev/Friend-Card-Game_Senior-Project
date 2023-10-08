ca25fe2b5cfeee48e8e7f03c220768467e4dbfcf
namekridchai <66484292+namekridchai@users.noreply.github.com>
2023-10-08 17:02:05 +0700

    style select card 2 version

--- FCG_front-end/src/App.jsx
+++ FCG_front-end/src/App.jsx
@@ -1,14 +1,16 @@
-import InGameInterface from "./interface/inGameInterface"
+// import InGameInterface from "./interface/inGameInterface"
 // import BiddingInterface from "./interface/BiddingInterface"
 // import InLobby from "./interface/InLobby"
 // import SummaryScore from "./interface/SummaryScore"
+import SelectCard from "./interface/SelectCard"
 function App() {
   return (
     <>
         {/* <BiddingInterface/> */}
-        <InGameInterface/>
+        {/* <InGameInterface/> */}
         {/* <InLobby/> */}
         {/* <SummaryScore/> */}
+        <SelectCard/>
         
     </>
     
