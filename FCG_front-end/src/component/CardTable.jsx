72a7c84b77b8cb1091f5a2ab0a710154c8a8cc82
namekridchai <66484292+namekridchai@users.noreply.github.com>
2023-10-14 00:35:37 +0700

    redo display score card

--- FCG_front-end/src/component/CardTable.jsx
+++ FCG_front-end/src/component/CardTable.jsx
@@ -1,41 +1,81 @@
 import './CardTable.css'
-
 function CardTable() {
+    const cardName = '9_of_clubs.svg'
+    const cardPath = "..\\public\\SVG-cards-1.3\\" + cardName
     return (
-        <table className='played_card text-xs text-center'>
-            <tr>
-                <td>♣️ </td>
-                <td>5</td>
-                <td>10</td>
-                <td>K</td>
+        <>
+            <div class="relative overflow-x-auto score_table border-double border-4 border-gray-600">
+                <table class="w-full text-sm text-left text-dark dark:text-gray-400">
+                    <thead class="text-xs  uppercase ">
+                        <tr >
+                            <th colSpan={5} className='text-center text-white bg-black text-base'>
+                                Score Card
+                            </th>
+                        </tr>
 
-              
-            </tr>
-            <tr>
-                <td>♠️</td>
-                <td>5</td>
-                <td>10</td>
-                <td></td>
-             
-            </tr>
-            <tr>
-                <td className='text-red-600'>♦️</td>
-                <td>5</td>
-                <td></td>
-                <td></td>
-             
-            </tr>
-            <tr>
-                <td className='text-red-600'>♥️</td>
-                <td>5</td>
-                <td>10</td>
-                <td>K</td>
-             
-            </tr>
-        </table>
-
-
-
+                    </thead>
+                    <tbody>
+                    <tr class="border-b dark:bg-gray-800 dark:border-gray-700 ">
+                           
+                           <td class="px-6 py-4">
+                               <img src={cardPath} alt="" />
+                           </td>
+                           <td class="px-6 py-4">
+                               <img src={cardPath} alt="" />
+                           </td>
+                           <td class="px-6 py-4">
+                               <img src={cardPath} alt="" />
+                           </td>
+                           
+                       </tr>
+                        <tr class="border-b dark:bg-gray-800 dark:border-gray-700 ">
+                           
+                            <td class="px-6 py-4">
+                                <img src={cardPath} alt="" />
+                            </td>
+                            <td class="px-6 py-4">
+                                <img src={cardPath} alt="" />
+                            </td>
+                            <td class="px-6 py-4">
+                                <img src={cardPath} alt="" />
+                            </td>
+                            
+                        </tr>
+                        <tr class="border-b dark:bg-gray-800 dark:border-gray-700 ">
+                           
+                           <td class="px-6 py-4">
+                               <img src={cardPath} alt="" />
+                           </td>
+                           <td class="px-6 py-4">
+                               <img src={cardPath} alt="" />
+                           </td>
+                           <td class="px-6 py-4">
+                               <img src={cardPath} alt="" />
+                           </td>
+                           
+                       </tr>
+                       <tr class="border-b dark:bg-gray-800 dark:border-gray-700">
+                           
+                           <td class="px-6 py-4">
+                               <img src={cardPath} alt="" />
+                           </td>
+                           <td class="px-6 py-4">
+                               <img src={cardPath} alt="" />
+                           </td>
+                           <td class="px-6 py-4">
+                               <img src={cardPath} alt="" />
+                           </td>
+                           
+                       </tr>
+                        
+                    </tbody>
+                </table>
+                <button class=" close_button bg-black  text-white font-bold py-2 px-4 border rounded-3xl">
+                    close
+                </button>
+            </div>
+        </>
     )
 }
+
 export default CardTable
