4de1d71b086b484fdc79fdee558b97e73fb040f9
KimChanaDev <chanaweestr@gmail.com>
2024-03-16 17:48:02 +0700

    change username to displayname of playerIngame that send to frontend

--- Server/src/Model/DTO/PlayerDTO.ts
+++ Server/src/Model/DTO/PlayerDTO.ts
@@ -12,6 +12,6 @@ export class PlayerDTO {
 	) {}
 
 	public static CreateFromPlayer(player: Player): PlayerDTO {
-		return new PlayerDTO(player.UID, player.username, player.GetIsReady(), player.isOwner, player.profileImagePath, player.GetIsDisconnected(), player.GetBotLevel());
+		return new PlayerDTO(player.UID, player.displayName, player.GetIsReady(), player.isOwner, player.profileImagePath, player.GetIsDisconnected(), player.GetBotLevel());
 	}
 }
